e65711f74e604b7aeb3a0ad39f91b1c3f40c839f
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-03-06 18:53:34 -0600

    Tailwind configurado en el proyecto

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 function App() {
   return (
     <div className="App">
-      <p>Hola budin</p>
+      <h1 className="text-3xl font-bold underline">
+        Hello Boc!
+      </h1>
     </div>
   );
 }
