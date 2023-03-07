3d4fcff850ece304367b252d0b60925c196ec798
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-03-07 10:34:30 -0600

    Realixzando pruebas

--- src/components/ModeCookies.jsx
+++ src/components/ModeCookies.jsx
@@ -5,17 +5,13 @@ export default function ModeCookies({url}) {
     
     async function handleSubmit(e){
         e.preventDefault()
-        console.log({url:e.target.urlInp.value ,name:e.target.cookieNameInp.value, content:e.target.cookieContentInp.value, domain:e.target.domainInp.value})
-        const response = await axios.post(url+e.target.urlInp.value,{
-            name:e.target.cookieNameInp.value,
-            content:e.target.cookieContentInp.value, 
-            domain:e.target.domainInp.value} ,{withCredentials:true})
+        console.log({url:e.target.urlInp.value , domain:e.target.domainInp.value})
+        const response = await axios.post(url+e.target.urlInp.value,{domain:e.target.domainInp.value} ,{withCredentials:true})
         console.log({url:url+e.target.urlInp.value, response})
 
     }
     async function handleSubmit2(e){
         e.preventDefault()
-        console.log(url+e.target.urlInp.value)
         const response = await axios.get(url+e.target.urlInp.value, {withCredentials:true})
         console.log(response)
     }
@@ -24,8 +20,6 @@ export default function ModeCookies({url}) {
     <div className='text-white bg-emerald-500 border-2 border-white p-5 flex flex-col items-center'>
     <form onSubmit={handleSubmit} className="flex flex-col">
         <input type="text" name="urlInp" className="rounded-lg text-emerald-900 m-4 focus:outline-none" placeholder='url'/>
-        <input type="text" name="cookieNameInp" className="rounded-lg text-emerald-900 m-4 focus:outline-none" placeholder='name'/>
-        <input type="text" name="cookieContentInp" className="rounded-lg text-emerald-900 m-4 focus:outline-none" placeholder='content'/>
         <input type="text" name="domainInp" className="rounded-lg text-emerald-900 m-4 focus:outline-none" placeholder='domain'/>
 
         <button className='p-1 border-2 border-white rounded-md'>Pedir cookie</button>
